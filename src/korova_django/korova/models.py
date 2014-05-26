e06cc99d0da31e81cd329a8b331181a27ec46358
Aloysio Figueiredo <aloysio@gmail.com>
2014-05-26 00:08:39 -0300

    Link operation ok

--- src/korova_django/korova/models.py
+++ src/korova_django/korova/models.py
@@ -69,7 +69,7 @@ class Account(models.Model):
     balance = models.DecimalField(max_digits=18, decimal_places=6)
     group = models.ForeignKey(Group, related_name='accounts', null=True)
     currency = models.ForeignKey(Currency)
-    type = EnumField(values=(
+    account_type = EnumField(values=(
                 'ASSET',
                 'LIABILITY',
                 'INCOME',
@@ -78,25 +78,37 @@ class Account(models.Model):
             ))
 
     @classmethod
-    def create(cls, code, name, profile, type, currency):
-        instance = cls(code=code, name=name, profile=profile, balance=0, type=type)
+    def create(cls, code, name, profile, account_type, currency):
+        instance = cls(code=code, name=name, profile=profile, balance=0, account_type=account_type)
         cur_obj = currencies[currency]
         is_foreign = False
         if profile.default_currency is not cur_obj:
             is_foreign = True
 
-        if is_foreign and ( type == 'INCOME' or type == 'EXPENSE'):
+        if is_foreign and ( account_type == 'INCOME' or account_type == 'EXPENSE'):
             raise KorovaError('A result account (INCOME | EXPENSE) cannot be in a foreign currency')
 
         return instance
 
-    # Find enough pockets to cover the requested amount
+    # Find enough pockets to cover the requested amount, if possible
+    # usage:
+    #   covered_amount, pocket_info = account.find_available_pockets(amount)
+    #   pocket_info is a tuple of (pocket_amount, pocket), where pocket_amount is the
+    #   amount to be deduced from pocket
     def find_available_pocket(self, amount):
-        available_pockets = []
-        try:
-            return self.pockets.filter(local_balance__gt=0).order_by('date')[0]
-        except IndexError:
-            return None
+        amount_to_cover = amount
+        ret_pockets = []
+        available_pockets = self.pockets.filter(local_balance__gt=0).order_by('date')
+
+        for pocket in available_pockets:
+            if pocket.foreign_amount >= amount_to_cover:
+                ret_pockets.append((pocket, amount_to_cover))
+                break
+            else:
+                amount_to_cover -= pocket.foreign_amount
+                ret_pockets.append((pocket, pocket.foreign_amount))
+
+        return amount_to_cover, ret_pockets
 
 class Pocket(models.Model):
     foreign_amount = models.DecimalField(max_digits=18, decimal_places=6)   # Creation amount in the account's currency
@@ -158,13 +170,70 @@ class Transaction(models.Model):
 
 
 class PocketOperation(models.Model):
-    type = EnumField(values=('CREATE', 'INCREASE', 'DECREASE'))
-    pocket = models.ForeignKey(Pocket,related_name='pocketOperations')
+    operation_type = EnumField(values=('CREATE', 'DECREASE'))
+    pocket = models.ForeignKey(Pocket)
+    split = models.ForeignKey(Split, related_name='pocketOperations')
 
 
 class Split(models.Model):
     amount = models.DecimalField(max_digits=18, decimal_places=6)
     account = models.ForeignKey(Account, null=True)
-    type = EnumField(values=('DEBIT','CREDIT'))
+    split_type = EnumField(values=('DEBIT', 'CREDIT'))
     date = models.DateTimeField()
     transaction = models.ForeignKey(Transaction, related_name='splits', null=True)
+    is_linked = False
+
+    @classmethod
+    def create(cls, amount, account, split_type, date):
+        instance = Split()
+        instance.amount = amount
+        instance.account = account
+        instance.split_type = split_type
+        instance.date = date
+
+    def link(self):
+        operation = None
+        local_currency_cost = 0
+
+        # First, deduce if we have to increase or decrease the account balance
+        if self.split_type == 'DEBIT':
+            if self.account.account_type in ('ASSET', 'EXPENSE'):
+                operation = 'CREATE'
+            else:
+                operation = 'DECREASE'
+        else:
+            if self.account.account_type in ('ASSET', 'EXPENSE'):
+                operation = 'DECREASE'
+            else:
+                operation = 'CREATE'
+
+
+        if operation == 'CREATE':
+            po = PocketOperation()
+            po.operation_type = 'CREATE'
+
+            p = Pocket()
+            p.account = self.account
+            p.foreign_amount = self.amount
+            p.date = self.date
+            p.foreign_balance = self.amount
+
+            po.pocket = p
+            po.split = self
+        else: ## 'DECREASE'
+            covered_amount, pocket_info = self.account.find_available_pocket(self.amount)
+
+            # for each element in pocket_info, we need to create a pocket operation
+            for amt, pocket in pocket_info:
+                pocket.foreign_balance -= amt
+                local_currency_cost += (pocket.local_amount*amt)/pocket.foreign_amount
+                po = PocketOperation()
+                po.pocket = pocket
+                po.operation_type = 'DECREASE'
+                po.split = self
+
+            # if there's any amount uncovered, we much add it to the account imbalance
+            if covered_amount < self.amount:
+                self.account.imbalance = self.amount - covered_amount
+
+        return local_currency_cost
